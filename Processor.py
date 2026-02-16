1b85d8dc7aafdf15e59f4465e34e0aa995eabd7c
a-b007 <anamitrabasu01@gmail.com>
2026-02-16 18:26:35 +0530

    changed string() to str()

---
+++ Processor.py
@@ -0,0 +1,100 @@
+def to_signed32(x):
+    x &= ((1 << 32) - 1)
+    return x if x < (1<<31) else x - (1 << 32)
+
+
+opcodes = {}
+
+
+
+class Register:
+    def __init__(self):
+        self.t = [0] * 10
+        self.s = [0] * 8
+        self.pc = 0
+
+        self.zero = 0
+        self.ra = 0
+        self.v0 = 0
+        self.v1 = 0
+
+
+
+class Memory:
+    
+    def __init__(self, file_data = "data.txt",file_instr = "instruction.txt"):
+        self.data_memory = file_data
+        self.instr_memory = file_instr
+        
+
+    def read_data(self, addr):
+        with open(self.data_memory, "r") as f:
+            for i, line in enumerate(f):
+                if i == addr:
+                    return to_signed32(int(line.strip(), 2))
+        return 0
+
+
+    def read_instr(self, addr):
+        with open(self.instr_memory, "r") as f:
+            for i, line in enumerate(f):
+                if i == addr:
+                    return to_signed32(int(line.strip(), 2))
+        return 0
+
+    def write_data(self, addr, value):
+        value &= ((1 << 32) - 1)
+
+        with open(self.data_memory, "r") as f:
+            lines = f.readlines()
+
+        while len(lines) <= addr:
+            lines.append("0".zfill(32) + "\n")
+
+        lines[addr] = format(value, "032b") + "\n"
+
+        with open(self.data_memory, "w") as f:
+            f.writelines(lines)
+
+
+    def write_instr(self, addr, value):
+        value &= ((1 << 32) - 1)
+
+        with open(self.instr_memory, "r") as f:
+            lines = f.readlines()
+
+        while len(lines) <= addr:
+            lines.append("0".zfill(32) + "\n")
+
+        lines[addr] = format(value, "032b") + "\n"
+
+        with open(self.instr_memory, "w") as f:
+            f.writelines(lines)
+
+
+
+class CPU:
+    def __init__(self):
+        self.mem = Memory()
+        self.reg = Register()
+        self.running = True
+        self.instr=""
+        self.data=""
+
+    def IF_stage(self):
+        self.instr = str(self.mem.read_instr(self.reg.pc//4))
+        self.reg.pc+=4
+
+
+    def ID_stage(self):
+        opcode = self.instr[0:6]
+
+    
+
+
+        
+        
+       
+        
+
+
