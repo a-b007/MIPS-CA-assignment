f42dcb4bde680fe33442a52e682f1f1bd029f772
Aaronya-C <aaronyachakraborty@gmail.com>
2026-02-21 12:50:34 +0530

    Update bubble_sort.asm

--- bubble_sort.asm
+++ bubble_sort.asm
@@ -14,21 +14,21 @@ main:
     ori  $t9, $t9, 0x0014
     lw   $a1, 0($t9)
 
-    addi $t0, $zero, 0        # i = 0
+    addi $t0, $zero, 0        #i = 0
 
 outer_loop:
-    addi $t7, $a1, -1         # n-1
+    addi $t7, $a1, -1         #n-1
     slt  $t8, $t0, $t7
-    beq  $t8, $zero, exit     # if i >= n-1 exit
+    beq  $t8, $zero, exit     #if i >= n-1 exit
 
-    addi $t1, $zero, 0        # j = 0
+    addi $t1, $zero, 0        #j = 0
 
 inner_loop:
-    sub  $t6, $a1, $t0        # n - i
-    addi $t6, $t6, -1         # n - i - 1
+    sub  $t6, $a1, $t0        #n - i
+    addi $t6, $t6, -1         #n - i - 1
 
     slt  $t8, $t1, $t6
-    beq  $t8, $zero, next_i   # if j >= n-i-1 go next i
+    beq  $t8, $zero, next_i   #if j >= n-i-1 go next i
 
     # Load A[j]
     sll  $t2, $t1, 2
@@ -42,7 +42,7 @@ inner_loop:
     lw   $t5, 0($t4)
 
     # if A[j] > A[j+1]
-    slt  $t8, $t5, $t3        # (A[j+1] < A[j])
+    slt  $t8, $t5, $t3        #(A[j+1] < A[j])
     beq  $t8, $zero, no_swap
 
     # swap
